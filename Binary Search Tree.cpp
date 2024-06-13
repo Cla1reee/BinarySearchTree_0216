5501869c97fd939584bca176efb4a4a6e6f84cd1
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2024-06-13 15:34:07 +0700

    menambahkan if dan membuat anak kiri dari parent menjadi node baru

--- Binary Search Tree.cpp
+++ Binary Search Tree.cpp
@@ -37,7 +37,14 @@ class BinaryTree {
 
         if (parent == nullptr) //If the parent is NULL (Tree is empty)
         {
-            ROOT = newNode; //
+            ROOT = newNode; //Mark the new node as ROOT
+            return; //Exit
+        }
+        
+        if (element < parent->info) //If the value in the data field of the new node is less than that of the parent 
+        {
+            parent->leftchild = newNode; //Make the left child of the parent point to the new node 
+
         }
     }
 };
