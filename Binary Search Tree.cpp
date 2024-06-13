f0951b8b5f1bf26d7d5d120c8ea9968fef9c461b
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2024-06-13 16:18:45 +0700

    perbaikan

--- Binary Search Tree.cpp
+++ Binary Search Tree.cpp
@@ -144,6 +144,11 @@ int main(){
             case'5':
             {
                 return 0;
+            default:
+            {
+                cout << "Invalid option" << endl;
+                break;
+            }
             }
         }
     }
