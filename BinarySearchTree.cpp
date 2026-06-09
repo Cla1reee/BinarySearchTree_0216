3d125e557f0b5cbe6cf30636e433faad154531f8
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2026-06-09 14:24:02 +0700

    Implement node insertion logic in BinaryTree class

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -31,5 +31,20 @@ class BinaryTree {
         Node* parent = nullptr;
         Node* currentNode = nullptr;
         search(element, parent, currentNode); //Locate the new node wich will be the parent of the node to be insert
+
+        if (parent == nullptr) //If the parent is NULL (Tree is empty)
+        {
+            ROOT = newNode; //Mark the new node as ROOT
+            return; //Exit
+        }
+        
+        if (element < parent->info) //If the value in the data field of the new node is less than that of the parent 
+        {
+            parent->leftchild = newNode; //Make the left child of the parent point to the new node 
+        }
+        else if (element > parent->info) //If the value in the data field of the new node is grater than that
+        {
+            parent->rightchild = newNode; //Make the right child of the parent point to the new node
+        }
     }
 };
