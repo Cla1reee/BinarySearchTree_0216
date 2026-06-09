614f2a28996e89ee164a583626f392cab1c34027
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2026-06-09 14:28:21 +0700

    Add switch case for user input handling in main function

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -108,3 +108,18 @@ int main(){
         cout << "4. Perform postorder traversal" << endl;
         cout << "5. Exit" << endl;
         cout << "\nEnter your choice (1-5) : ";
+
+        char ch;
+        cin >> ch;
+        cout << endl;
+
+        switch (ch)
+        {
+            case '1':
+            {
+                cout << "Enter a word: ";
+                string word;
+                cin >> word;
+                x.insert(word);
+                break;
+            }
